d376861bc72918f7b637f9627696b52ba2d07cca
nirongxu <595485548@qq.com>
2021-07-22 15:25:16 +0800

    集成脚手架编译

--- configTemplate.js
+++ configTemplate.js
@@ -8,37 +8,19 @@ module.exports = [
     {
         type: 'input',
         name: 'name',
-        message: 'project name',
+        message: '项目名称',
         default: 'zy-vue-template-cli',
     },
     {
         type: 'input',
         name: 'description',
-        message: 'description',
-        default: '通过zyjk-cli可以定制化编译成项目需要的专属模板',
+        message: '项目描述',
+        default: 'electron-vue2-template 默认模板',
     },
     {
         type: 'input',
         name: 'author',
-        message: 'author',
-        default: 'zyjk',
-    },
-    {
-        type: 'confirm',
-        name: 'router',
-        message: '是否安装router',
-        default: true,
-    },
-    {
-        type: 'confirm',
-        name: 'eslint',
-        message: '是否安装eslint',
-        default: true,
-    },
-    {
-        type: 'confirm',
-        name: 'typescript',
-        message: '是否使用typescript',
-        default: true,
-    },
+        message: '创建者',
+        default: 'NiRongxu',
+    }
 ];
