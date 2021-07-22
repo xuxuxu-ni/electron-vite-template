ecbee5d0930f29cc39d4970dec93512c13358763
nirongxu <595485548@qq.com>
2021-07-22 17:01:27 +0800

    生产环境关闭控制台

--- src/render/views/About.vue
+++ src/render/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{projectName}}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      projectName: 'electron-vue-template'
+    }
+  }
+}
+</script>
